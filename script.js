c75ed183a2bd7ddeade1fb1202c4cb6703883dd7
Ruhi K <ruhik8789@gmail.com>
2025-08-07 22:52:45 +0530

    Added animation while hovering over.

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var elemC = document.querySelectorAll(".elem");
+var elemContainer = document.querySelector("#elem-container");
+console.log(elemC);
+var fixed = document.querySelector("#fixed-image");
+elemC.forEach(element => {
+    element.addEventListener("mouseenter", function() {
+        var image = element.getAttribute("data-image");
+        fixed.style.display = "block";
+        fixed.style.backgroundImage = `url(${image})`;
+        fixed.style.backgroundSize = "cover";
+        fixed.style.backgroundPosition = "center";
+    });
+});
+elemContainer.addEventListener("mouseleave", function() {
+    fixed.style.display = "none";
+})
