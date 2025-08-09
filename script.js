e04070ef82bc136506f888c4c3b04e2233f963ad
Ruhi K <ruhik8789@gmail.com>
2025-08-09 23:38:33 +0530

    Added swiper to the UI.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
 
 var elemC = document.querySelectorAll(".elem");
