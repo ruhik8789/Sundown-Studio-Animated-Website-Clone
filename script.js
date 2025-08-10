8ec8a65368fdf3ed4fc5419fbd6cc1c434200907
Ruhi K <ruhik8789@gmail.com>
2025-08-10 20:30:46 +0530

    animation changes

--- script.js
+++ script.js
@@ -3,29 +3,39 @@
 //     smooth: true
 // });
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-var elemC = document.querySelectorAll(".elem");
-var elemContainer = document.querySelector("#elem-container");
-console.log(elemC);
-var fixed = document.querySelector("#fixed-image");
-elemC.forEach(element => {
-    element.addEventListener("mouseenter", function() {
-        var image = element.getAttribute("data-image");
-        fixed.style.display = "block";
-        fixed.style.backgroundImage = `url(${image})`;
-        fixed.style.backgroundSize = "cover";
-        fixed.style.backgroundPosition = "center";
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
     });
-});
-elemContainer.addEventListener("mouseleave", function() {
-    fixed.style.display = "none";
+}
+
+function page4Animation() {
+    var elemC = document.querySelectorAll(".elem");
+    var elemContainer = document.querySelector("#elem-container");
+    console.log(elemC);
+    var fixed = document.querySelector("#fixed-image");
+    elemC.forEach(element => {
+        element.addEventListener("mouseenter", function () {
+            var image = element.getAttribute("data-image");
+            fixed.style.display = "block";
+            fixed.style.backgroundImage = `url(${image})`;
+            fixed.style.backgroundSize = "cover";
+            fixed.style.backgroundPosition = "center";
+        });
+    });
+    elemContainer.addEventListener("mouseleave", function () {
+        fixed.style.display = "none";
+    })
+}
+
+swiperAnimation();
+
+page4Animation();
+
+var menu = document.querySelector("nav h3");
+menu.addEventListener("click", () => {
+    var menuDiv = document.querySelector("#full-scr");
+    menuDiv.style.top = "0%"
 })
